1ed8ee4eb1ea65b11384171997114ed627f02302
KrausC2 <curtis.kraus2002@outlook.com>
2026-01-30 09:27:24 +1100

    feat: Add placeholder pages & nav element

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -5,5 +5,14 @@
 	let { children } = $props();
 </script>
 
+<nav>
+	<a href="/">Home</a>
+	<a href="/now">Now</a>
+	<a href="/things">Things</a>
+	<a href="/articles">Articles</a>
+	<a href="/projects">Projects</a>
+</nav>
+
+
 <svelte:head><link rel="icon" href={favicon} /></svelte:head>
 {@render children()}
