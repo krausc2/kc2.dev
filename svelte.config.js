80e677ac6488b9b882f2d555e69b099343a8223e
KrausC2 <curtis.kraus2002@outlook.com>
2026-02-04 07:56:36 +1100

    build: restore vitePreprocess

--- svelte.config.js
+++ svelte.config.js
@@ -1,11 +1,12 @@
 import { mdsvex } from 'mdsvex';
 import adapter from '@sveltejs/adapter-node';
+import { vitePreprocess } from '@sveltejs/kit/vite';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: { adapter: adapter() },
-	preprocess: [mdsvex()],
-	extensions: ['.svelte', '.svx']
+	preprocess: [mdsvex({ extensions: ['.md'] }), vitePreprocess()],
+	extensions: ['.svelte', '.md']
 };
 
 export default config;
