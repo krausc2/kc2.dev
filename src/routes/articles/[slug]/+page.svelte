471a115104fd7ed55bf742dbc038bb8efe80613f
KrausC2 <curtis.kraus2002@outlook.com>
2026-02-04 08:02:10 +1100

    feat: basic slug loading function for blog

---
+++ src/routes/articles/[slug]/+page.svelte
@@ -0,0 +1,25 @@
+<script>
+  /*
+    Passes data from load function to the page as data prop,
+    from +page.ts.
+
+    Anywhere data.meta.option or data.content is used, Svelte
+    will update the value.
+  */
+  let { data } = $props();
+</script>
+
+<article>
+  <h1>
+    {data.meta.title}
+  </h1>
+
+  <h2>
+    {data.meta.date}
+  </h2>
+
+  <div> 
+    <data.content />
+  </div>
+
+</article>
--- src/routes/articles/[slug]/+page.ts
+++ src/routes/articles/[slug]/+page.ts
@@ -2,11 +2,12 @@ import { error } from '@sveltejs/kit';
 
 export async function load({ params }) {
     try {
+      // Dynamically import file from lib based on the slug parameter
       const article = await import(`$lib/articles/${params.slug}.md`);
 
       return {
-        content: article.default, // Passes body of file
-        meta: article.metadata // Passes frontmatter data
+        content: article.default, // Markdown content
+        meta: article.metadata // Frontmatter (title, date, etc.)
       }
     }catch (err) {
       // Resource not found
