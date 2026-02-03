1b5ded13bf9fc93152f2258d5ac7f8d04e6d2a58
KrausC2 <curtis.kraus2002@outlook.com>
2026-02-04 08:01:11 +1100

    style: rearrange index page & add html tags

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -5,17 +5,25 @@
 	let { children } = $props();
 </script>
 
-<nav>
-	<a href="/">Home</a>
-	<a href="/now">Now</a>
-	<a href="/things">Things</a>
-	<a href="/articles">Articles</a>
-	<a href="/projects">Projects</a>
-</nav>
+<header>
+	<nav>
+		<a href="/">Home</a>
+		<a href="/now">Now</a>
+		<a href="/things">Things</a>
+		<a href="/articles">Articles</a>
+		<a href="/projects">Projects</a>
+	</nav>
+</header>
 
-<div class="footer">
-	<p>© 2026 KC². Source available at <a href="https://github.com/krausc2/kc2.dev">GitHub</a>.</p>
-</div>
+<main>
+	{@render children()}
+</main>
+
+<footer>
+	<div>
+		<p>© 2026 KC². Source available at <a href="https://github.com/krausc2/kc2.dev">GitHub</a>.</p>
+	</div>
+</footer>
+
 
 <svelte:head><link rel="icon" href={favicon} /></svelte:head>
-{@render children()}
