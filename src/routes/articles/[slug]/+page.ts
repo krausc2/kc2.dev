de8e99efbd5baebe3c49c8666ae4ca5308e0f980
KrausC2 <curtis.kraus2002@outlook.com>
2026-02-14 12:46:39 +1100

    wip: improve data handling logic for article pages

--- src/routes/articles/[slug]/+page.ts
+++ src/routes/articles/[slug]/+page.ts
@@ -2,15 +2,19 @@ import { error } from '@sveltejs/kit';
 
 export async function load({ params }) {
     try {
-      // Dynamically import file from lib based on the slug parameter
+      // Dynamically import files from lib based on the slug parameter
       const article = await import(`$lib/articles/${params.slug}.md`);
 
       return {
-        content: article.default, // Markdown content
-        meta: article.metadata // Frontmatter (title, date, etc.)
+        content: article.default,
+        // Spread metadata, but convert date
+        meta: {
+          ...article.metadata,
+          date: new Date(article.metadata.date)
+        }
       }
+    // For resource that is not found, display 404
     }catch (err) {
-      // Resource not found
       throw error (404, `Could not find ${params.slug}`);
     }
 }
