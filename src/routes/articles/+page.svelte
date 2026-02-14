c4fc145cfa5f319612174a64c7db83aa3c6c2726
KrausC2 <curtis.kraus2002@outlook.com>
2026-02-14 12:43:17 +1100

    wip: add placeholder blog list

---
+++ src/routes/articles/+page.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+	let { data } = $props();
+</script>
+
+<div>
+	{#each data.articles as article}
+		<a href="/articles/{article.slug}">
+      
+			<h1>{article.title}</h1>
+
+			<h2>{article.date.toLocaleDateString(undefined, { dateStyle: 'long' })}</h2>
+
+			{#if article.tags.length > 0}
+				{#each article.tags as tag}
+					<h2>{tag}</h2>
+				{/each}
+			{/if}
+		</a>
+	{/each}
+</div>
