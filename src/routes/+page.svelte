b1a88703742bcb86d7c80c18778b43bdecbf0193
kc2dev <curtis.kraus2002@outlook.com>
2026-03-31 13:59:39 +1100

    feat: change formatting scheme for Home page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -6,9 +6,17 @@
 	<p class="text-5xl font-bold">krausc2</p>
 	<p class="font-mono text-stone-400">// {@html getSplash("day")} <span class="cursor">|</span></p>
 
-	<div class="grid grid-cols-1 gap-8 lg:grid-cols-2">
-		<p>G'day, I'm Curtis.</p>
-		<div class="flex aspect-3/4 w-full items-center justify-center bg-stone-400">
+	<div class="flex flex-col gap-8 lg:flex-row">
+		<div class="flex flex-1 flex-col gap-8">
+			<p>
+				G'day, I'm Curtis, a Cyber Security Specialist and Undergraduate Student at Macquarie
+				University, Sydney.
+			</p>
+			<p>
+				Test of second paragraph.
+			</p>
+		</div>
+		<div class="flex aspect-3/4 w-full flex-1 items-center justify-center bg-stone-400">
 			<p class="text-stone-500">Placeholder Image</p>
 		</div>
 	</div>
