c4b3e44d983ec1fe62c3c2c520b2ad1879a7ac86
kc2dev <curtis.kraus2002@outlook.com>
2026-04-03 21:40:23 +1100

    fix: wrap text under image when window is small enough

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,24 +2,22 @@
 	import { getSplash } from "$lib/splashes";
 </script>
 
-<section class="flex flex-col gap-8 mb-32">
-	<p class="text-5xl font-bold">krausc2</p>
-	<p class="font-mono text-custom-coral">// {@html getSplash("day")} <span class="cursor">|</span></p>
+<section class="flex flex-col mb-32">
+	<p class="text-5xl font-bold leading-none mb-4">krausc2</p>
+	<p class="font-mono text-custom-coral mb-12">// {@html getSplash("day")} <span class="cursor">|</span></p>
 
-	<div class="flex flex-col gap-8 lg:flex-row lg:items-start">
-		<div class="flex flex-1 flex-col gap-8">
-			<p>
+	<div class="flex flex-col lg:block">
+		<div class="order-last flex aspect-3/4 items-center justify-center bg-stone-400 lg:float-right lg:ml-8 lg:mb-4 lg:w-1/2">
+			<p class="text-stone-500">Placeholder Image</p>
+		</div>
+		<div class="flex flex-col gap-8 lg:contents">
+			<p class="lg:mb-8">
 				G'day, I'm <b>Curtis</b>, a <b>Cyber Security Specialist</b> and <b>Undergraduate</b> at Macquarie University, Sydney.
 			</p>
 			<p>
 				My current role is as a Cyber Security Engineer (internal consulting), with a background in Systems Administration and IT Support for large independent schools, alongside roles as an Electronics Repair Technician and Systems Integrator.
 			</p>
-			<p>
-
-			</p>
-		</div>
-		<div class="flex aspect-3/4 flex-1 items-center justify-center bg-stone-400">
-			<p class="text-stone-500">Placeholder Image</p>
+			<p></p>
 		</div>
 	</div>
 </section>
