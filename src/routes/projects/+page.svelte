8d1a9adb749a416546ece657daccf22447d5ae49
KrausC2 <curtis.kraus2002@outlook.com>
2026-06-25 14:26:37 +1000

    chore: remove placeholder page content

--- src/routes/projects/+page.svelte
+++ src/routes/projects/+page.svelte
@@ -1,10 +1,3 @@
 <section class="mb-32 flex flex-col">
 	<p class="mb-4 border-b pb-4 text-5xl leading-none font-bold">projects</p>
-
-	<p>🕷️🕸️ Hmmm... it's a bit quiet here</p>
-	<br />
-	<p>
-		Check <a href="https://github.com/krausc2" target="_blank" rel="noreferrer">GitHub</a> to see what
-		I'm up to
-	</p>
 </section>
