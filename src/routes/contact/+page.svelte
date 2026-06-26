13d45f4856a6aa4406e29c8f3e896b5080f0b3d3
KrausC2 <curtis.kraus2002@outlook.com>
2026-06-26 14:27:18 +1000

    docs: add todos

--- src/routes/contact/+page.svelte
+++ src/routes/contact/+page.svelte
@@ -1,4 +1,4 @@
 <script lang="ts">
-    // TODO Add contact information
-    // TODO Add a contact form but make it in the format of a terminal command line prompt
+	// TODO Add contact information
+	// TODO Add a contact form but make it in the format of a terminal command line prompt
 </script>
