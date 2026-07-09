44d9ae547840b3727592324ac3bab971be495d85
KrausC2 <curtis.kraus2002@outlook.com>
2026-07-09 22:05:34 +1000

    fix: remove custom text colour

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -114,7 +114,7 @@
 					: 'opacity-0'}"
 			/>
 		</div>
-		<figcaption class="mt-4 text-center font-mono text-base text-custom-coral">
+		<figcaption class="mt-4 text-center font-mono text-base">
 			Stylish as always, circa 2014.
 		</figcaption>
 	</figure>
