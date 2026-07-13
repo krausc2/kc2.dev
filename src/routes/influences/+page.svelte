1a69fb57f1b455c8b7e4e573bb2237282cac73cd
KrausC2 <curtis.kraus2002@outlook.com>
2026-07-13 15:53:01 +1000

    feat: add draft structure and description for new page

--- src/routes/influences/+page.svelte
+++ src/routes/influences/+page.svelte
@@ -1,3 +1,28 @@
 <section class="mb-32 flex flex-col">
-	<p class="mb-4 border-b pb-4 text-5xl leading-none font-bold">influences</p>
+	<p class="border-b pb-4 text-5xl leading-none font-bold">influences</p>
+
+	<div class="flex flex-col gap-8 border-b border-stone-300 pt-4 pb-4">
+		<p>
+			An <span class="font-bold">/influences</span> page was an idea that came to me after reflecting on the many books, albums, and games I've found meaningful in some way.
+		</p>
+		<p>
+			I wanted a place to collate, share, and reflect on how these influences have shaped my worldview; for <i>better</i> or for <i>worse</i>, they have driven me <i>towards</i> or <i>away</i> from certain ideas and ways of thinking. It's not a reading list, nor a picture board, and not a music playlist, but rather a curated map of influences (though it can certainly be made up of those things).
+		</p>
+		<p>
+			If you want to read about how to create your own influences page, you can find out more in <a href="$lib/articles/influences" class="text-custom-coral hover:underline">this article</a>.
+		</p>
+	</div>
+
+	<section class="pt-16 gap-8">
+		<span class="text-xl font-bold border-b">books</span>
+	</section>
+
+	<section class="pt-16 gap-8">
+		<span class="text-xl font-bold border-b">music</span>
+	</section>
+
+	<section class="pt-16 gap-8">
+		<span class="text-xl font-bold border-b">games</span>
+	</section>
 </section>
+
