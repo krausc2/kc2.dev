c384f33d175f041e2841342d53d75033ad89fa87
KrausC2 <curtis.kraus2002@outlook.com>
2026-07-22 20:58:52 +1000

    feat: add back button [<] to article page

--- src/routes/articles/[slug]/+page.svelte
+++ src/routes/articles/[slug]/+page.svelte
@@ -10,7 +10,15 @@ can never clip the mobile layout.
 	let { data } = $props();
 </script>
 
-<p class="pb-4 text-5xl leading-none font-bold invisible">articles</p>
+<div class="relative">
+	<a
+		href="/articles"
+		class="absolute top-0 left-0 z-50 flex h-12 w-12 cursor-pointer items-center justify-center border bg-stone-100 font-mono text-lg text-stone-900 transition-colors duration-700 hover:text-stone-500 hover:duration-200"
+	>
+		<span>&lt;</span>
+	</a>
+	<p class="invisible pb-4 text-5xl leading-none font-bold">"title"</p>
+</div>
 
 <article class="border-t pt-4">
 	<h1>
