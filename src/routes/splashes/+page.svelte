275b7e1757037019981977e1c0264ab1635ecaee
KrausC2 <curtis.kraus2002@outlook.com>
2026-07-25 23:58:17 +1000

    feat: add page description

--- src/routes/splashes/+page.svelte
+++ src/routes/splashes/+page.svelte
@@ -4,8 +4,11 @@
 
 <section class="mb-32 flex flex-col">
 	<p class="border-b pb-4 text-5xl leading-none font-bold">splashes</p>
+	<div class="pt-4 gap-8 flex flex-col border-b pb-4">
+		<p>What is a splash? Well, to me it's a short concept or excerpt from a larger piece of writing; not quite a quote, though it <span class="italic">can</span> be one. Its true origin more likely comes from the early web where webmasters would add an MOTD to the top of a page.</p>
 
-	<div class="pt-4">
-		<p>What is a splash?</p>
+		<p>My first exposure to the idea was in 2012 where they existed as short, punchy messages on the title screen of the video game Minecraft. I later saw the idea in the title bar of the video game Terraria, where they were called "Intro Messages".</p>
+
+		<p>In all of this, my idea was to collect provocative, memorable, and motivating phrases from the media I come across. For this reason, I don't endorse all of them, but they're all useful for something. Spend some time watching them tick by, or scroll down to cheat your way through.</p>
     </div>
 </section>
