0a31ae08126e47fdf8ea1a6bce38ae5855cbac3a
KrausC2 <curtis.kraus2002@outlook.com>
2026-07-25 23:14:19 +1000

    feat: add new draft for splashes description page

---
+++ src/routes/splashes/+page.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+    import { createSplash } from '$lib/splashes.svelte'
+</script>
+
+<section class="mb-32 flex flex-col">
+	<p class="border-b pb-4 text-5xl leading-none font-bold">splashes</p>
+
+	<div class="pt-4">
+		<p>What is a splash?</p>
+    </div>
+</section>
