5bdb444e1be1907b24d39493b53325818464ff44
KrausC2 <curtis.kraus2002@outlook.com>
2026-07-28 09:20:51 +1000

    feat: add custom SVG components

---
+++ src/lib/components/ui/svg-icons/External.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+	let { size = 14, strokeWidth = 2.5, class: className = "", ...restProps } = $props();
+</script>
+
+<svg
+	xmlns="http://www.w3.org/2000/svg"
+	width={size}
+	height={size}
+	viewBox="0 0 24 24"
+	fill="none"
+	stroke="currentColor"
+	stroke-width={strokeWidth}
+	stroke-linecap="round"
+	stroke-linejoin="round"
+	class={className}
+	{...restProps}
+>
+	<path d="M15 3h6v6" />
+	<path d="M10 14 21 3" />
+	<path d="M18 13v6a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V8a2 2 0 0 1 2-2h6" />
+</svg>
---
+++ src/lib/components/ui/svg-icons/MoreInfo.svelte
@@ -0,0 +1,20 @@
+<script>
+	let { size = 14, strokeWidth = 2.5, class: className = "", ...restProps } = $props();
+</script>
+
+<svg
+	xmlns="http://www.w3.org/2000/svg"
+	width={size}
+	height={size}
+	viewBox="0 0 24 24"
+	fill="none"
+	stroke="currentColor"
+	stroke-width={strokeWidth}
+	stroke-linecap="round"
+	stroke-linejoin="round"
+	class={className}
+	{...restProps}
+	><circle cx="12" cy="12" r="10" /><path d="M9.09 9a3 3 0 0 1 5.83 1c0 2-3 3-3 3" /><path
+		d="M12 17h.01"
+	/></svg
+>
