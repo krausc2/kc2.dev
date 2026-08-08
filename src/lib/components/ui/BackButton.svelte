cddf622fcef9c8929a449ed0db89484975600ce8
KrausC2 <curtis.kraus2002@outlook.com>
2026-08-08 18:36:21 +1000

    feat: create standard back button component

---
+++ src/lib/components/ui/BackButton.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+	import { Button } from "$lib/components/ui/button";
+
+	let { href = "/" } = $props<{ href: string }>();
+</script>
+
+<Button
+	{href}
+	variant="ghost"
+	class="absolute top-0 left-0 z-50 flex h-12 w-12 cursor-pointer items-center justify-center border bg-stone-100 font-mono text-lg text-stone-900 transition-colors duration-700 hover:text-stone-500 hover:duration-200"
+>
+	&lt;
+</Button>
