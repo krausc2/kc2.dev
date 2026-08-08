4ae1f2d9f78d38f6dbae3f3023887f9eab277a8d
KrausC2 <curtis.kraus2002@outlook.com>
2026-08-08 19:17:59 +1000

    refactor: replace hard-coded button with new component

--- src/routes/articles/[slug]/+page.svelte
+++ src/routes/articles/[slug]/+page.svelte
@@ -1,16 +1,12 @@
-<script>
+<script lang="ts">
 	import { formatDate } from "$lib/utils";
+	import BackButton from "$lib/components/ui/BackButton.svelte";
 
 	let { data } = $props();
 </script>
 
 <div class="relative">
-	<a
-		href="/articles"
-		class="absolute top-0 left-0 z-50 flex h-12 w-12 cursor-pointer items-center justify-center border bg-stone-100 font-mono text-lg text-stone-900 transition-colors duration-700 hover:text-stone-500 hover:duration-200"
-	>
-		<span>&lt;</span>
-	</a>
+	<BackButton href="/articles" />
 	<p class="invisible pb-4 text-5xl leading-none font-bold">"title"</p>
 </div>
 
