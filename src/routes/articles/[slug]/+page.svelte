e01dfc7c87f431acf7d727af01df5b25626c8165
KrausC2 <curtis.kraus2002@outlook.com>
2026-08-12 22:08:38 +1000

    feat: improve .article-body styles for readability

--- src/routes/articles/[slug]/+page.svelte
+++ src/routes/articles/[slug]/+page.svelte
@@ -10,12 +10,14 @@
 	<p class="invisible pb-4 text-5xl leading-none font-bold">"title"</p>
 </div>
 
-<article class="flex flex-col gap-4 border-t pt-4">
-	<p class="text-2xl font-bold">{data.meta.title}</p>
+<article class="flex flex-col border-t">
+	<div class="mx-auto w-full max-w-[70ch]">
+		<p class="text-2xl font-bold pt-16 text-left">{data.meta.title}</p>
 
-	<p>{formatDate(data.meta.date)}</p>
+		<p class="text-sm pb-8">{formatDate(data.meta.date)}</p>
 
-	<div class="article-body">
-		<data.content />
+		<div class="article-body">
+			<data.content />
+		</div>
 	</div>
 </article>
